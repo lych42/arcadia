8131ec93f69a309852086b6b36795a4f116df11c
guerincharly@sfr.fr <lannister135@gmail.com>
2024-11-05 14:42:45 +0100

    maj services

--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ServiceRepository;
 
 class ServicesController extends AbstractController
 {
-    #[Route('/services', name: 'app_services')]
+    #[Route('/services', name: 'services')]
     public function index(): Response
     {
+        $services = [ ['name' => 'Visites Guidées', 'description' => 'Des visites guidées avec des experts du zoo.'], ['name' => 'Soins aux Animaux', 'description' => 'Découvrez comment nous prenons soin de nos animaux.'], ];
+
         return $this->render('services/index.html.twig', [
-            'controller_name' => 'ServicesController',
+            'services' => $services,
         ]);
     }
 }
