19eed95070802d2e48e54082ee2f037cd7ce9f14
guerincharly@sfr.fr <lannister135@gmail.com>
2024-11-05 14:01:01 +0100

    add contact controller

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -11,6 +11,7 @@ class ContactController extends AbstractController
     #[Route('/contact', name: 'app_contact')]
     public function index(): Response
     {
+
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
         ]);
