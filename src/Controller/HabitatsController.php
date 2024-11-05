014ea63b5d52030cd5cb77d790f7a141398cd897
guerincharly@sfr.fr <lannister135@gmail.com>
2024-11-05 14:37:18 +0100

    maj habitats

--- src/Controller/HabitatsController.php
+++ src/Controller/HabitatsController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\HabitatRepository;
 
 class HabitatsController extends AbstractController
 {
-    #[Route('/habitats', name: 'app_habitats')]
-    public function index(): Response
-    {
+    #[Route('/habitats', name: 'habitats')]
+    public function index(HabitatRepository $habitatRepository): Response
+    {    
+        $habitats = $habitatRepository->findAll();
+        
         return $this->render('habitats/index.html.twig', [
-            'controller_name' => 'HabitatsController',
+            'habitats' => $habitats,
         ]);
     }
 }
